fdf040a69e480620d23bc6fc11999024ca013bc7
Niteshgs2 <niteshsawant94@gmail.com>
2018-06-28 21:02:20 -0400

    Delete Adjacent element if Duplicate in String

--- DeleteAdj.java
+++ DeleteAdj.java
@@ -10,7 +10,6 @@ public class deleteAdj {
         for (int i = 1; i < str.length(); i++) {
             if (str.charAt(i) == str.charAt(i - 1)) {
                 str = str.substring(0, i - 1) + str.substring(i + 1);
-
             }
         }
         if (str.length() == 0) {
