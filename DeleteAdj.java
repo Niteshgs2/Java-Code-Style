94a02cef9c9fdaf01e2475fb6666cae264ff4142
Niteshgs2 <niteshsawant94@gmail.com>
2018-06-28 20:49:52 -0400

    Delet Duplicate Adjacent Alphabet

---
+++ DeleteAdj.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Nitesh on 3/23/2018.
+ */
+import java.util.*;
+public class deleteAdj {
+
+    public static void main(String[] args) {
+        String str1 = "aacVBcccdD";
+        String str = str1.toLowerCase();
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                str = str.substring(0, i - 1) + str.substring(i + 1);
+
+            }
+        }
+        if (str.length() == 0) {
+            System.out.println("Empty String");
+        } else {
+            System.out.println(str);
+        }
+    }
+}
